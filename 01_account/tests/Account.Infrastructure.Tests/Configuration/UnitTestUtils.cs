55e874c45e4ae10a0df858d60b06acad9e24f390
FR\jnantenaina <jnantenaina@kpmg.fr>
2024-02-05 17:24:14 +0100

    feat: add repository unit test

---
+++ 01_account/tests/Account.Infrastructure.Tests/Configuration/UnitTestUtils.cs
@@ -0,0 +1,75 @@
+﻿// <copyright file="UnitTestUtils.cs" company="KPMG">
+// Copyright (c) KPMG. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using AutoFixture;
+using Kpmg.Account.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using MockQueryable.Moq;
+using Moq;
+using Pulse.Account.Infrastructure.Context;
+using Pulse.Account.Infrastructure.Entities;
+
+namespace Kpmg.Account.Infrastructure.Tests.Configuration
+{
+    [ExcludeFromCodeCoverage]
+    public class UnitTestUtils
+    {
+        public static AccountRepository InitAccountRepository(Fixture fixture, List<TAccount> accountsModel)
+        {
+            var rolesModel = accountsModel.SelectMany(item => item.TRoles).ToList();
+            var deploymentsModel = fixture.Create<List<TDeploymentPlanning>>().AsQueryable();
+            var addressModel = fixture.Create<List<TAddress>>().AsQueryable();
+            var phoneModel = fixture.Create<List<TPhone>>().AsQueryable();
+            var contactModel = fixture.Create<List<TContact>>();
+
+            accountsModel.ForEach(itemAccount =>
+            {
+                foreach (var itemRole in itemAccount.TRoles)
+                {
+                    itemRole.AccountId = itemAccount.AccountId;
+                }
+            });
+            rolesModel.ForEach(item => item.ContactId = 123);
+            contactModel.ForEach(item => item.ContactId = 123);
+            var contactModelIQueryable = contactModel.AsQueryable();
+            var rolesModelIQueryable = rolesModel.AsQueryable();
+            var accountModelIQueryable = accountsModel.AsQueryable().BuildMock();
+
+            var accountMockSet = new Mock<DbSet<TAccount>>();
+            accountMockSet.As<IQueryable<TAccount>>().Setup(m => m.Provider).Returns(accountModelIQueryable.Provider);
+            accountMockSet.As<IQueryable<TAccount>>().Setup(m => m.Expression).Returns(accountModelIQueryable.Expression);
+
+            var roleMockSet = new Mock<DbSet<TRoles>>();
+            roleMockSet.As<IQueryable<TRoles>>().Setup(m => m.Provider).Returns(rolesModelIQueryable.Provider);
+            roleMockSet.As<IQueryable<TRoles>>().Setup(m => m.Expression).Returns(rolesModelIQueryable.Expression);
+
+            var deploymentMockSet = new Mock<DbSet<TDeploymentPlanning>>();
+            deploymentMockSet.As<IQueryable<TDeploymentPlanning>>().Setup(m => m.Provider).Returns(deploymentsModel.Provider);
+            deploymentMockSet.As<IQueryable<TDeploymentPlanning>>().Setup(m => m.Expression).Returns(deploymentsModel.Expression);
+
+            var addressMockSet = new Mock<DbSet<TAddress>>();
+            addressMockSet.As<IQueryable<TAddress>>().Setup(m => m.Provider).Returns(addressModel.Provider);
+            addressMockSet.As<IQueryable<TAddress>>().Setup(m => m.Expression).Returns(addressModel.Expression);
+
+            var contactMockSet = new Mock<DbSet<TContact>>();
+            contactMockSet.As<IQueryable<TAddress>>().Setup(m => m.Provider).Returns(contactModelIQueryable.Provider);
+            contactMockSet.As<IQueryable<TAddress>>().Setup(m => m.Expression).Returns(contactModelIQueryable.Expression);
+
+            var phoneMockSet = new Mock<DbSet<TPhone>>();
+            phoneMockSet.As<IQueryable<TPhone>>().Setup(m => m.Provider).Returns(phoneModel.Provider);
+            phoneMockSet.As<IQueryable<TPhone>>().Setup(m => m.Expression).Returns(phoneModel.Expression);
+
+            var mockContext = new Mock<AccountContext>();
+            mockContext.Setup(m => m.TAccount).Returns(accountMockSet.Object);
+            mockContext.Setup(m => m.TRoles).Returns(roleMockSet.Object);
+            mockContext.Setup(m => m.TDeploymentPlanning).Returns(deploymentMockSet.Object);
+            mockContext.Setup(m => m.TAddress).Returns(addressMockSet.Object);
+            mockContext.Setup(m => m.TContact).Returns(contactMockSet.Object);
+            mockContext.Setup(m => m.TPhone).Returns(phoneMockSet.Object);
+
+            return new AccountRepository(mockContext.Object);
+        }
+    }
+}
--- 01_account/tests/Account.Infrastructure.Tests/Repositories/AccountRepositoryTests.cs
+++ 01_account/tests/Account.Infrastructure.Tests/Repositories/AccountRepositoryTests.cs
@@ -2,19 +2,72 @@
 // Copyright (c) KPMG. All rights reserved.
 // </copyright>
 
+using System.Text.Json;
 using AutoFixture;
+using Kpmg.Account.Infrastructure.Tests.Configuration;
+using Newtonsoft.Json;
+using Pulse.Account.Core.Models.Utils;
+using Pulse.Account.Infrastructure.Entities;
+using AccountModel = Kpmg.Account.Core.Models.Account;
 
 namespace Kpmg.Account.Infrastructure.Tests.Repositories
 {
     public class AccountRepositoryTests
     {
         private readonly Fixture _fixture;
+        private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
 
         public AccountRepositoryTests()
         {
             _fixture = new Fixture();
             _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList().ForEach(b => _fixture.Behaviors.Remove(b));
             _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        [Fact]
+        public async Task Should_GetAccountList_ReturnsOkResultAsync()
+        {
+            // Arrange
+            var accountsModel = _fixture.Create<List<TAccount>>();
+            var accountRepository = UnitTestUtils.InitAccountRepository(_fixture, accountsModel);
+            var accountObject = accountsModel.Select(item => item.TAccountToAccountModel(123));
+            Paging<AccountModel> accountPaging = new Paging<AccountModel>()
+            {
+                CurrentPage = 1,
+                Items = accountObject,
+                TotalItems = accountObject.Count(),
+                TotalPage = 1
+            };
+
+            // Act
+            var accounts = await accountRepository.GetAccountsAsync(search: string.Empty, contactId: 123, page: 1, limit: 4);
+
+            // Assert
+            var accountExpect = JsonConvert.SerializeObject(accountPaging);
+            var accountReceived = JsonConvert.SerializeObject(accounts);
+            Assert.Equal(accountExpect, accountReceived);
+        }
+
+        [Fact]
+        public async Task Should_GetAccountDetail_ReturnsOkResultAsync()
+        {
+            // Arrange
+            var accountsModel = _fixture.Create<List<TAccount>>();
+            var accountFirst = accountsModel.FirstOrDefault();
+            var accountDetail = accountFirst?.TAccountToAccountDetail();
+            var accountRepository = UnitTestUtils.InitAccountRepository(_fixture, accountsModel);
+
+            // Act
+            var accounts = await accountRepository.GetAccountDetailAsync(accountFirst.AccountGlobalUniqueId);
+
+            // Assert
+            Assert.Equal(accountDetail.AccountNumber, accounts.AccountNumber);
+            Assert.Equal(accountDetail.AccountId, accounts.AccountId);
+            Assert.Equal(accountDetail.Legal?.LegalName, accounts.Legal?.LegalName);
         }
     }
 }
