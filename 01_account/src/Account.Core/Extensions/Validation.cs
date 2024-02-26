823f84d1e1833f9377a365ca6c7e4cd935a291f8
baotrungnguyen <baotrungnguyen@kpmg.fr>
2024-02-26 11:39:07 +0100

    refactor: traite comment

---
+++ 01_account/src/Account.Core/Extensions/Validation.cs
@@ -0,0 +1,17 @@
+﻿using Pulse.Account.Core.Requests;
+
+namespace Pulse.Account.Core.Extensions
+{
+    public static class Validation
+    {
+        public static bool ValidateDateDelegation(CreateDelegation delegation)
+        {
+            if (delegation.EndDate.CompareTo(delegation.StartDate) < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- 01_account/src/Account.Core/Services/DelegationService.cs
+++ 01_account/src/Account.Core/Services/DelegationService.cs
@@ -4,6 +4,7 @@
 
 using Kpmg.ExceptionMiddleware.AdvancedException;
 using Pulse.Account.Core.Exceptions;
+using Pulse.Account.Core.Extensions;
 using Pulse.Account.Core.Interfaces;
 using Pulse.Account.Core.Models;
 using Pulse.Account.Core.Requests;
@@ -21,7 +22,7 @@ public class DelegationService : IDelegationService
 
     public async Task<int> CreateDelegationAsync(CreateDelegation delegation)
     {
-        if (!ValidateDateDelegation(delegation))
+        if (!Validation.ValidateDateDelegation(delegation))
         {
             throw new BadRequestException(Errors.DelegationDateInvalidCode, Errors.DelegationDateInvalidMessage);
         }
@@ -37,15 +38,5 @@ public class DelegationService : IDelegationService
     public async Task<IReadOnlyCollection<Delegation>> GetDelegationsAsync(int delegatorId, int delegateeId)
     {
         return await _delegationRepository.GetDelegationsAsync(delegatorId, delegateeId);
-    }
-
-    private static bool ValidateDateDelegation(CreateDelegation delegation)
-    {
-        if (delegation.EndDate.CompareTo(delegation.StartDate) < 0)
-        {
-            return false;
-        }
-
-        return true;
     }
 }
