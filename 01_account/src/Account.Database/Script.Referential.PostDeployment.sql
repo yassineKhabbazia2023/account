669f725bd7a067b6b34f0ce4acc6480d8904c05d
Bao Trung Nguyen <baotrungnguyen@kpmg.fr>
2024-04-08 12:40:04 +0000

    Merged PR 30381: refactor: maj script postDeployment
    refactor: maj script postDeployment
    
    Related work items: #313211

--- 01_account/src/Account.Database/Script.Referential.PostDeployment.sql
+++ 01_account/src/Account.Database/Script.Referential.PostDeployment.sql
@@ -12,11 +12,11 @@ Post-Deployment Script Template
 
 :r .\postDeployment\Hub.Refential.sql
 :r .\postDeployment\Script.Naf.sql
-:r .\postDeployment\Script.Account.sql
-:r .\postDeployment\Script.Contact.sql
-:r .\postDeployment\Script.Address.sql
+:r .\postDeployment\Script.600Accounts.sql
+:r .\postDeployment\Script.600Address.sql
+:r .\postDeployment\Script.Contact.Pulse.sql
+:r .\postDeployment\Script.Contact.Client.sql
+:r .\postDeployment\Script.Role.Pulse.sql
+:r .\postDeployment\Script.Role.Client.sql
 :r .\postDeployment\Script.Phone.sql
-:r .\postDeployment\Script.Role.sql
 :r .\postDeployment\Script.Deployment.sql
-:r .\postDeployment\Script.Delegation.sql
-:r .\postDeployment\Script.DelegationDetail.sql
