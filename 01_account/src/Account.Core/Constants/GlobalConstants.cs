11dd25c69566122f70c64ae4d2162ee5b5d1c68a
FR\jnantenaina <jnantenaina@kpmg.fr>
2024-05-16 12:10:15 +0200

    feat: add status filter on delegation list

--- 01_account/src/Account.Core/Constants/GlobalConstants.cs
+++ 01_account/src/Account.Core/Constants/GlobalConstants.cs
@@ -7,5 +7,6 @@ namespace Pulse.Account.Core.Constants
     public static class GlobalConstants
     {
         public static readonly int RETRYTIMESPAN = 3000;
+        public static readonly string DisableDelegation = "disabled";
     }
 }
