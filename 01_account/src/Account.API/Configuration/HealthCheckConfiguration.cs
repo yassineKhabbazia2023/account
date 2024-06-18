e68f58c46313a6a749d3a087dbdedb84c0ba47ca
Fory, Clement <cfory@kpmg.fr>
2024-06-18 14:34:23 +0000

    Merged PR 32342: fix(config): modification de la config HealthCheck
    fix(config): modification de la config HealthCheck
    
    Related work items: #324165

--- 01_account/src/Account.API/Configuration/HealthCheckConfiguration.cs
+++ 01_account/src/Account.API/Configuration/HealthCheckConfiguration.cs
@@ -9,20 +9,9 @@ namespace Pulse.Account.API.Configuration
 {
     public static class HealthCheckConfiguration
     {
-        public static void ConfigureHealthCheckService(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddHealthChecks();
-            services.AddHealthChecksUI()
-                    .AddInMemoryStorage();
-            if (configuration != null)
-            {
-                services.Configure<HealthChecks.UI.Data.HealthCheckConfiguration>(configuration.GetSection("HealthCheck"));
-            }
-        }
-
         public static void UseHealthcheckUI(IApplicationBuilder app)
         {
-            app.UseHealthChecks("/api/health", new HealthCheckOptions
+            app.UseHealthChecks("/health", new HealthCheckOptions
             {
                 ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
             });
