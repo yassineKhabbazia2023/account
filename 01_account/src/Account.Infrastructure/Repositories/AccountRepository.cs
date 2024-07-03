e88591d28b563e6b98bc8764b8238cd7f7e78cd9
Eddy Eyme-Rossat <eeyme@kmpg.fr>
2024-07-03 14:33:49 +0200

    fix: 325168, concatenates firstname + lastname for search

--- 01_account/src/Account.Infrastructure/Repositories/AccountRepository.cs
+++ 01_account/src/Account.Infrastructure/Repositories/AccountRepository.cs
@@ -61,8 +61,7 @@ namespace Pulse.Account.Infrastructure.Repositories
                     query = from n in query
                             where n.LegalName.ToLower().Contains(criteria.Search)
                                   || n.AccountNumber.ToLower().Contains(criteria.Search)
-                                  || n.RoleEntity.Any(role => role.IsSignatory == true && (role.Contact.FirstName.ToLower().Contains(criteria.Search)
-                                                      || role.Contact.LastName.ToLower().Contains(criteria.Search)
+                                  || n.RoleEntity.Any(role => role.IsSignatory == true && (($"{role.Contact.FirstName} {role.Contact.LastName}").ToLower().Contains(criteria.Search.ToLower())
                                                       || role.Contact.Email.ToLower().Contains(criteria.Search)))
                             select n;
                 }
