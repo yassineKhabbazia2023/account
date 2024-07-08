646ef75ced3f32de6a63e6e13c8ab8d28e659429
Fory, Clement <cfory@kpmg.fr>
2024-07-08 12:04:58 +0200

    fix bug portefeuille

--- 01_account/src/Account.Infrastructure/Mappers/MapAccountDbToAccountModel.cs
+++ 01_account/src/Account.Infrastructure/Mappers/MapAccountDbToAccountModel.cs
@@ -56,7 +56,7 @@ namespace Pulse.Account.Infrastructure.Mappers
                 return null;
             }
 
-            var signatory = source.RoleEntity?.FirstOrDefault(r => r.IsSignatory == true && int.Parse(r.Contact.Type) != (int)ContactType.Collaborator);
+            var signatory = source.RoleEntity?.FirstOrDefault(r => r.IsSignatory == true && r.Contact.Type != ContactType.Collaborator.ToString());
             var currentContact = source.RoleEntity?.FirstOrDefault(r => r.ContactId == contactId);
 
             return
