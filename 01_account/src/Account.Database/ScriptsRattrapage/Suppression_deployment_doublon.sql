c04858560c63856b63a7d5b7d7e85dc467e02a12
FORY, Clément <cfory@rydge.fr>
2025-03-04 10:27:04 +0000

    Merged PR 2955: feat(sql): ajout du script de vérif/suppression des deploymentsen doublon
    feat(sql): ajout du script de vérif/suppression des deploymentsen doublon
    
    Related work items: #43940

---
+++ 01_account/src/Account.Database/ScriptsRattrapage/Suppression_deployment_doublon.sql
@@ -0,0 +1,38 @@
+﻿------------------- VERIFICATION -------------------
+WITH RankedDeployments AS (
+    SELECT 
+        DeploymentId,
+        AccountId,
+        DeploymentDate,
+        Status,
+        ROW_NUMBER() OVER (PARTITION BY AccountId ORDER BY Status DESC, DeploymentDate DESC) AS rn
+    FROM 
+        account.Deployment
+)
+SELECT * FROM account.Deployment
+WHERE DeploymentId IN (
+    SELECT DeploymentId
+    FROM RankedDeployments
+    WHERE rn > 1
+)
+ORDER BY AccountId;
+
+
+
+------------------- SUPPRESSION -------------------
+WITH RankedDeployments AS (
+    SELECT 
+        DeploymentId,
+        AccountId,
+        DeploymentDate,
+        Status,
+        ROW_NUMBER() OVER (PARTITION BY AccountId ORDER BY Status DESC, DeploymentDate DESC) AS rn
+    FROM 
+        account.Deployment
+)
+DELETE FROM account.Deployment
+WHERE DeploymentId IN (
+    SELECT DeploymentId
+    FROM RankedDeployments
+    WHERE rn > 1
+);
