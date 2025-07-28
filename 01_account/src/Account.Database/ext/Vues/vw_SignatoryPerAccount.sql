8c7a96f903995e04f7c672d8b7f342856f22f752
DBIRA Houssem <hdbira@rydge.fr>
2025-07-28 13:04:20 +0000

    Merged PR 5699: Ajout de la vue vw_SignatoryPerAccount et schéma ext
    Attendu :
    ![image.png](https://dev.azure.com/FR-TPEME/fb2de3b3-d9ff-42db-abfe-2a7530c44fad/_apis/git/repositories/340e27a6-afad-4cd3-afb9-9df52edf9d09/pullRequests/5699/attachments/image.png)
    
    Ajout de la vue vw_SignatoryPerAccount et schéma ext
    
    Modifications du fichier `Account.Database.sqlproj` pour inclure un nouveau dossier `ext` et un sous-dossier `Vues`, contenant le fichier de construction `vw_SignatoryPerAccount.sql`.
    
    Création d'une nouvelle vue dans `vw_SignatoryPerAccount.sql` pour sélectionner les signataires associés à chaque compte, en utilisant une fonction de classement.
    
    Ajout d'un schéma `ext` dans le fichier `ext.sql`.
    
    Related work items: #51650

---
+++ 01_account/src/Account.Database/ext/Vues/vw_SignatoryPerAccount.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [ext].[vw_SignatoryPerAccount] AS
+WITH SignatoryRank AS (
+    SELECT 
+        cnt.FirstName,
+        cnt.LastName,
+        cnt.Email,
+        acc.AccountNumber,
+        ROW_NUMBER() OVER (
+            PARTITION BY acc.AccountNumber
+            ORDER BY cnt.CreationDate ASC
+        ) AS RowNum
+    FROM [account].[Role] accRole
+    JOIN account.Account acc ON acc.AccountId = accRole.AccountId
+    JOIN actor.Contact cnt ON cnt.ContactId = accRole.ContactId
+    WHERE accRole.IsSignatory = 1
+        AND cnt.Type = 'Customer'
+        AND cnt.IsActive = 1
+)
+SELECT 
+    FirstName,
+    LastName,
+    Email,
+    AccountNumber
+FROM SignatoryRank
+WHERE RowNum = 1;
