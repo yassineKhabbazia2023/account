ced7b825b4bd8d696ca58c331f81bff7408d5524
AIT OUARABI Arab <aaitouarabi@rydge.fr>
2025-09-02 14:13:26 +0000

    Merged PR 6226: feat(sql): ajout de la table OfferEligibility
    feat(sql): ajout de la table OfferEligibility
    
    Related work items: #53393

---
+++ 01_account/src/Account.Database/account/Tables/OfferEligibility.sql
@@ -0,0 +1,48 @@
+﻿CREATE TABLE [account].[OfferEligibility]
+(
+    [AccountId]   INT           NOT NULL,
+    [OfferName]   VARCHAR(100)  NOT NULL,
+    [IsEligible]  BIT           NOT NULL,
+    [ApprovedDate] DATETIME2    NOT NULL,
+    [ApprovedBy]  VARCHAR(255)  NOT NULL,
+    CONSTRAINT [C_AccountId_PK] 
+        PRIMARY KEY CLUSTERED ([AccountId] ASC),
+    CONSTRAINT [C_Account_OfferEligibility_FK] 
+        FOREIGN KEY ([AccountId]) REFERENCES [account].[Account]([AccountId])
+);
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Identifiant technique du compte (clé primaire et étrangère vers account.Account)',
+    @level0type = N'SCHEMA', @level0name = N'account',
+    @level1type = N'TABLE',  @level1name = N'OfferEligibility',
+    @level2type = N'COLUMN', @level2name = N'AccountId';
+GO
+
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Nom de l''offre associée à ce compte',
+    @level0type = N'SCHEMA', @level0name = N'account',
+    @level1type = N'TABLE',  @level1name = N'OfferEligibility',
+    @level2type = N'COLUMN', @level2name = N'OfferName';
+GO
+
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Indique si le compte est éligible à l''offre (1 = Oui, 0 = Non)',
+    @level0type = N'SCHEMA', @level0name = N'account',
+    @level1type = N'TABLE',  @level1name = N'OfferEligibility',
+    @level2type = N'COLUMN', @level2name = N'IsEligible';
+GO
+
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Date de validation de l''offre',
+    @level0type = N'SCHEMA', @level0name = N'account',
+    @level1type = N'TABLE',  @level1name = N'OfferEligibility',
+    @level2type = N'COLUMN', @level2name = N'ApprovedDate';
+GO
+
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Utilisateur ou processus ayant validé l''offre',
+    @level0type = N'SCHEMA', @level0name = N'account',
+    @level1type = N'TABLE',  @level1name = N'OfferEligibility',
+    @level2type = N'COLUMN', @level2name = N'ApprovedBy';
+GO
