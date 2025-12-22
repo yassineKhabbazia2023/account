19eb5b439b870f899a79b0b08787e6773586cee7
BEN AMOR Oussema <obenamor@rydge.fr>
2025-12-22 14:53:15 +0000

    Merged PR 7343: chore: update hub names #57168
    chore: update hub names #57168
    
    Related work items: #57168

---
+++ 01_account/src/Account.Database/Scripts/57168_Reverse_Update_Hub_Names.sql
@@ -0,0 +1,82 @@
+﻿delete from account.Hub
+
+SET IDENTITY_INSERT [account].[Hub] ON 
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (1, N'Hub Aquitaine Nord')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (2, N'Hub Languedoc-Roussillon')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (3, N'Hub Tarn Aveyron')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (4, N'Hub Toulouse Midi Pyrennées')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (5, N'Hub Pays Basque Landes')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (6, N'Hub Atlantique Ouest')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (7, N'Hub Anjou Maine')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (8, N'Hub Charente Limousin')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (9, N'Hub Haute Bretagne')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (10, N'Hub Nantes Atlantique Vendée')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (11, N'Hub Bretagne Océan')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (12, N'Hub La Réunion')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (13, N'Hub Grand Ouest Parisien')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (14, N'Hub Centre')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (15, N'Hub Marne et Oise')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (16, N'Hub Berry Nivernais')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (17, N'Hub Paris')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (18, N'Hub Arc Alpin')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (19, N'Hub Loire & Drôme Ardèche')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (20, N'Hub Franche-Comté Bourgogne Nord')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (21, N'Hub Ain et Lyonnais')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (22, N'Hub Bourgogne Sud')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (23, N'Hub Auvergne')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (24, N'Hub Provence Côte d''Azur')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (25, N'Hub Normandie Seine Baie de Somme')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (26, N'Hub Antilles Guyane')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (27, N'Hub Caen Sud Normandie')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (28, N'Hub Manche')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (29, N'Hub Amiens Haute Picardie')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (30, N'Hub Littoral')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (31, N'Hub Artois Grand Hainaut')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (32, N'Hub Estuaire Terre et Mer')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (33, N'Hub Lille Métropole')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (34, N'Hub Alsace Nord')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (35, N'Hub Lorraine Sud')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (36, N'Hub Alsace Sud')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (37, N'Hub Champagne-Ardenne')
+GO
+INSERT [account].[Hub] ([HubId], [HubName]) VALUES (38, N'Hub Lorraine Nord')
+GO
+SET IDENTITY_INSERT [account].[Hub] OFF
+GO
