f2ab214529e05a3cfb8be7624d5c35ff2f7d62c2
DBIRA Houssem <hdbira@rydge.fr>
2026-03-11 09:49:37 +0000

    Merged PR 8324: feat(referential-infos): ajouter les données statiques des dropdowns contact...
    feat(referential-infos): ajouter les données statiques des dropdowns contact et compte
    ![image (2).png](https://dev.azure.com/FR-TPEME/fb2de3b3-d9ff-42db-abfe-2a7530c44fad/_apis/git/repositories/340e27a6-afad-4cd3-afb9-9df52edf9d09/pullRequests/8324/attachments/image%20%282%29.png)
    
    Related work items: #59649

--- 01_account/src/Account.Core/Models/AccountReferentialInformation.cs
+++ 01_account/src/Account.Core/Models/AccountReferentialInformation.cs
@@ -120,4 +120,154 @@ public class AccountReferentialInformation
             };
         }
     }
+
+    public IReadOnlyDictionary<string, string> LegalForm
+    {
+        get
+        {
+            return new Dictionary<string, string>
+            {
+                { "ADM - ETAT", "ADMINISTRATION - ETAT" },
+                { "ASS. CULTUELLE", "CONGRÉGATION OU ASSOCIATION CULTUELLE" },
+                { "ASS. DE DROIT LOCAL", "ASSOCIATION DE DROIT LOCAL" },
+                { "ASS. DÉCLARÉE", "ASSOCIATION DÉCLARÉE" },
+                { "ASS. INS ECO", "ASSOCIATION D'INSERTION PAR L'ÉCONOMIQUE" },
+                { "ASS. INTERM", "ASSOCIATION INTERMÉDIAIRE" },
+                { "ASS. RUP", "ASSOCIATION RECONNUE D'UTILITÉ PUBLIQUE" },
+                { "AUTO", "AUTO - AUTO ENTREPRENEUR" },
+                { "AUTRE", "AUTRE" },
+                { "AUTRE COOP", "AUTRE COOPÉRATIVE" },
+                { "AUTRE SC", "AUTRE SOCIÉTÉ CIVILE" },
+                { "CAISSE EP_PREV", "CAISSE D'EPARGNE ET DE PREVOYANCE" },
+                { "COL TERR", "COLLECTIVITÉ TERRITORIALE" },
+                { "COM. ENTR", "COMITÉ D' ENTREPRISE" },
+                { "EARL", "EARL - ENTREPRISE AGRICOLE À RESPONSABIL" },
+                { "EIRL", "EIRL" },
+                { "EN COURS", "EN COURS D'IMMATRICULATION" },
+                { "ENT IND", "ENTREPRISE INDIVIDUELLE" },
+                { "EPA", "EPA - ETABLISSEMENT PUBLIC ADMINISTRATIF" },
+                { "EPIC", "EPIC - ETABLISST PUBLIC INDUSTRIEL ET CO" },
+                { "EPL", "EPL - ENTREPRISE PUBLIQUE LOCALE" },
+                { "EURL", "EURL - ENTREPRISE UNIPERSONNELLE À RESPO" },
+                { "FIDUCIE", "FIDUCIE" },
+                { "FOND. ABRITÉE", "FONDATION ABRITÉE" },
+                { "FOND. ENT", "FONDATION D'ENTREPRISE" },
+                { "FOND. HOSPI", "FONDATION HOSPITALIÈRE" },
+                { "FOND. PART", "FONDATION PARTENARIALE" },
+                { "FOND. RUP", "FONDATION RECONNUE D'UTILITÉ PUBLIQUE" },
+                { "FOND. UNIVERS", "FONDATION UNIVERSITAIRE" },
+                { "FONDS COOP SCIENT", "FONDS DE COOPÉRATION SCIENTIFIQUE" },
+                { "FONDS DOTAT", "FONDS DE DOTATION" },
+                { "FONDS PEREN ECO", "FONDS DE PERENNITE ECONOMIQUE" },
+                { "GAEC", "GAEC - GRPT EXPLOITATION AGRICOLE COMMUN" },
+                { "GCS_GCSMS PRIV", "GCS & GCSMS DROIT PRIVÉ" },
+                { "GCS_GCSMS PUB", "GCS & GCSMS DROIT PUBLIC" },
+                { "GFA", "GFA - GRPT FONCIER AGRICOLE" },
+                { "GIP", "GIP - GROUPEMENT D'INTERÊT PUBLIC" },
+                { "GROUP EMPL", "GROUPEMENT D'EMPLOYEURS" },
+                { "GRPT", "GRPT INTÉRÊT ÉCONOMIQUE" },
+                { "IND", "INDIVISION" },
+                { "OPCVM", "OPCVM" },
+                { "ORDRE PRO", "ORDRE PROFESSIONNEL" },
+                { "ORG MUTUALISTE", "ORGANISME MUTUALISTE" },
+                { "ORG SÉCU SOC RET", "ORGANISME DE SÉCURITÉ SOCIALE, RETRAITE" },
+                { "ORG SYNDICALE", "ORGANISATION SYNDICALE" },
+                { "ORGCONSULAIRE", "ORGANISME CONSULAIRE" },
+                { "PART", "PARTICULIER" },
+                { "PMDE", "PERSONNE MORALE DE DROIT ÉTRANGER" },
+                { "SA À CONS ADM", "SA À CONSEIL D'ADMINISTRATION" },
+                { "SA À DIR ET CONS", "SA À DIRECTOIRE ET CONSEIL DE SURVEILLAN" },
+                { "SARL", "SARL - SOCIÉTÉ À RESPONSABILITÉ LIMITÉ" },
+                { "SAS", "SAS - SOCIÉTÉ PAR ACTIONS SIMPLIFIÉE" },
+                { "SCA", "SCA - COMMANDITE PAR ACTIONS" },
+                { "SCEA", "SCEA - STE CIVILE EXPLOITATION AGRICOLE" },
+                { "SCI", "SCI - STE CIVILE IMMOBILIÈRE" },
+                { "SCIC", "SOCIÉTÉ COOPÉRATIVE D'INTÉRÊT COLLECTIF" },
+                { "SCOP", "SCOP - SOCIÉTÉ COOPÉRATIVE ET PARTICIPA" },
+                { "SCP", "SCP - STE CIVILE PROFESSIONNELLE" },
+                { "SCPI", "SCPI - SOCIETE CIVILE PLACEMENT IMMOBILI" },
+                { "SCS", "SCS - COMMANDITE SIMPLE" },
+                { "SE", "SE - SOCIÉTÉ EUROPÉENNE" },
+                { "SEL  UNIPERS", "SEL - SOCIETE D'EXERCICE LIBÉRAL UNIPERS" },
+                { "SEL À RESP LIM", "SEL À RESPONSABILITÉ LIMITÉE" },
+                { "SEL COM ACT", "SEL EN COMMANDITE PAR ACTIONS" },
+                { "SEL FA", "SEL À FORME ANONYME" },
+                { "SEL PAR ACT SIMP", "SEL PAR ACTIONS SIMPLIFIÉES" },
+                { "SFAIT", "SFAIT - SOCIÉTÉ DE FAIT" },
+                { "SISA", "SISA - STE INTERPROF SOINS AMBULATOIRS" },
+                { "SNC", "SNC - SOCIÉTÉ EN NOM COLLECTIF" },
+                { "SOC CIV MOY", "SOCIÉTE CIVILE DE MOYENS" },
+                { "SOC COOP AGR", "SOCIÉTÉ COOPÉRATIVE AGRICOLE" },
+                { "SOC COOP COM", "SOCIÉTÉ COOPÉRATIVE COMMERCIALE" },
+                { "SOC ETRANGERE", "SOCIETE ETRANGERE" },
+                { "SPART", "SPART - SOCIÉTÉ EN PARTICIPATION" },
+            };
+        }
+    }
+
+    public IReadOnlyDictionary<string, string> ContactTitle
+    {
+        get
+        {
+            return new Dictionary<string, string>
+            {
+                { "M", "Monsieur" },
+                { "Mme", "Madame" },
+                { "Dr", "Docteur" },
+                { "Pr", "Professeur" }
+            };
+        }
+    }
+
+    public IReadOnlyDictionary<string, string> ContactService
+    {
+        get
+        {
+            return new Dictionary<string, string>
+            {
+                { "Achats", "Achats" },
+                { "Autre", "Autre" },
+                { "Commercial, Vente", "Commercial, Vente" },
+                { "Comptabilité", "Comptabilité" },
+                { "Consolidation", "Consolidation" },
+                { "DF", "Direction financière" },
+                { "DG", "Direction générale" },
+                { "Juridique", "Juridique" },
+                { "Marketing /Com", "Marketing / Communication" },
+                { "R&D / Innovation", "R&D / Innovation" },
+                { "RH-Formation", "Ressources Humaines Formation" },
+                { "RH-Général", "Ressources Humaines Général" },
+                { "RH-Paie", "Ressources Humaines Paie" },
+                { "RSE", "RSE" },
+                { "SI/IT", "Systèmes d'informations / IT" },
+            };
+        }
+    }
+
+    public IReadOnlyDictionary<string, string> ContactHierarchicalLevel
+    {
+        get
+        {
+            return new Dictionary<string, string>
+            {
+                { "Autre", "Autre" },
+                { "Collaborateur", "Collaborateur" },
+                { "Directeur", "Directeur" },
+                { "Président / Gérant", "Président / Gérant" },
+                { "Responsable", "Responsable" },
+            };
+        }
+    }
+
+    public IReadOnlyDictionary<string, string> LegalStructure
+    {
+        get
+        {
+            return new Dictionary<string, string>
+            {
+                { "PersonneMorale", "Personne morale" },
+                { "PersonnePhysique", "Personne physique" },
+            };
+        }
+    }
 }
